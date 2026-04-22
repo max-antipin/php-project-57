8919beab7e9267bf557132b326e23faa934f9e44
Max Antipin <max.v.antipin@gmail.com>
2026-04-22 11:33:50 +0300

    Fix HTTPS in production environment

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -19,6 +20,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        if ($this->app->isProduction()) {
+            URL::forceHttps(true);
+            $this->app['request']->server->set('HTTPS', 'on');
+            $this->app['request']->server->set('HTTP_X_FORWARDED_PROTO', 'https');
+        }
     }
 }
